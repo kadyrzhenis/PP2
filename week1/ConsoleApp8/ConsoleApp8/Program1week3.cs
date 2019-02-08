bc752b73e15a65b1e4d886638152487864f35b75
kadyrzhenis <47256333+kadyrzhenis@users.noreply.github.com>
2019-02-09 00:58:54 +0600

    Add files via upload

---
+++ week1/ConsoleApp8/ConsoleApp8/Program1week3.cs
@@ -0,0 +1,32 @@
+﻿using System; 
+ using System.Collections.Generic; 
+ using System.Linq; 
+ using System.Text; 
+ using System.Threading.Tasks;  
+ 
+ namespace Example6
+{ 
+     class Program
+     { /// <summary>
+     /// САНДАРДЫ ДУБЛИКАТТАП ШЫҒАРУ
+     /// </summary>
+     /// <param name="args"></param>
+         static void Main(string[] args)
+         {
+            int n = int.Parse(Console.ReadLine());//САН ЕНГІЗУ
+            string s = Console.ReadLine();
+            string[] arr = s.Split(' ');//АРАСЫ ПРОБЕЛМЕН КЕЛГЕН САНДАРДЫ ЖАЗУ
+          
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                for (int j = 0; j < 2; ++j)//САНДАРДЫ ДУБЛИКАТТАДЫҚ
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+            Console.ReadKey();
+              
+         } 
+     } 
+ } 
