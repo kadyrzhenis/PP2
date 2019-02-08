8007eff54d76f3be833a4204483fd50ef376256a
kadyrzhenis <47256333+kadyrzhenis@users.noreply.github.com>
2019-02-08 23:10:09 +0600

    Add files via upload

---
+++ Program1week3.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+ using System.Collections.Generic; 
+ using System.Linq; 
+ using System.Text; 
+ using System.Threading.Tasks;  
+ 
+ namespace Example6
+{ 
+     class Program
+     { 
+         static void Main(string[] args)
+         {
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            string[] arr = s.Split(' ');
+            // int[] a = new int[n];
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                for (int j = 0; j < 2; ++j)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+            Console.ReadKey();
+              
+         } 
+     } 
+ } 
