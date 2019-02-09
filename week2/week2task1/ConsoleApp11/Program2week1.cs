c2592349bd1d0401afb54e95adb85e33c3e29335
kadyrzhenis <47256333+kadyrzhenis@users.noreply.github.com>
2019-02-09 11:25:42 +0600

    Add files via upload

---
+++ week2/week2task1/ConsoleApp11/Program2week1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] arr = s.Split(' ');
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - i -1])
+                {
+                    Console.WriteLine("No");
+
+                }
+                else
+                {
+                    Console.WriteLine("Yes");
+                }
+                Console.ReadKey();
+            }
+            
+                }
+                
+            }
+        }
+   
+
